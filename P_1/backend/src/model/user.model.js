65307495a9438fdd138883ae3f651a9bd2b314b6
sham jadhav <ghanshamjadhav2003@gmail.com>
2026-07-28 20:49:09 +0530

    two file add

--- P_1/backend/src/model/user.model.js
+++ P_1/backend/src/model/user.model.js
@@ -1,9 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose({
+const userSchema = new mongoose(
+  {
+    username: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true },
+);
 
-})
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
-const userModel = model('user', userSchema)
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const userModel = model("user", userSchema);
 
 export default userModel;
